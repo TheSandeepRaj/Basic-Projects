47435e5217ecb1ff380157a552438e2728c76866
TheSandeepRaj <sandeeprajghimire@gmail.com>
2020-09-04 13:24:47 +0545

    added new functions and hangman pic

--- Hangman/hangman.py
+++ Hangman/hangman.py
@@ -1,15 +1,58 @@
 # https://en.wikipedia.org/wiki/Hangman_(game)
-# https://inventwithpython.com/hangman.py
 # Layout - Keypad, Word in progress, 
 # ToDOs: Ask for how many guess u  want, guesses remain, 
 #       prev guess
-
-
-
-
+#commonly used letters — E, S, T, M, and N 
 
 import random
+hangman_pic = ['''
 
+   ______________
+    |         |
+    |         
+    |        
+    |        
+    |
+==================''', '''
+
+   ______________
+    |         |
+    |         0
+    |  
+    |  
+    |
+==================''', '''
+
+   ______________
+    |         |
+    |         0
+    |         |
+    |   
+    |
+==================''', '''
+
+   ______________
+    |         |
+    |         0
+    |        /|
+    |        
+    |
+==================''', '''
+
+   ______________
+    |         |
+    |         0
+    |        /|\
+    |      
+    |
+==================''', '''
+
+   ______________
+    |         |
+    |         0
+    |        /|\
+    |        / 
+==================''', '''
 
    ______________
     |         |
@@ -17,15 +60,35 @@ import random
     |        /|\
     |        / \
     |
-    |
-=======================
+==================''', '''
+
+''']
+
+# List of word list based on category 
+words_animal = ('Ant Baboon Badger Bat Bear Beaver Camel Cat Clam Cobra Cougar Coyote Crow'
+                'Deer Dog Donkey Duck Eagle Ferret Fox Frog Goat Goose Hawk Lion Lizard Llama'
+                'Mole Monkey Moose Mouse Mule Newt Otter Owl Panda Parrot Pigeon Python Rabbit'
+                'Ram Rat Raven Rhino Salmon Seal Shark Sheep Skunk Sloth Snake Spider Stork'
+                'Swan Tiger Toad Trout Turkey Turtle Weasel Whale Wolf Wombat Zebra').split()
+
+# words_eng = ('Instantaneous Heavily Moderately Essential Feature Consumption Dissipation'
+#             'Actuate Prescribed Designate Overwhelmed Tremendous Inconsequential Accumulation'
+#             'Intrinsic Diffuse Bias Convey Constraints Monetary Catastrophe').split()
+
+# words_easy = 
+
+# words_medium =
+
+# words_hard = 
+
+# words_expert =
 
 
-#Word bank of animals
-words = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
-         'coyote crow deer dog donkey duck eagle ferret fox frog goat '
-         'goose hawk lion lizard llama mole monkey moose mouse mule newt '
-         'otter owl panda parrot pigeon python rabbit ram rat raven '
-         'rhino salmon seal shark sheep skunk sloth snake spider '
-         'stork swan tiger toad trout turkey turtle weasel whale wolf '
-         'wombat zebra ').split()
+def get_words(word_list):
+    # TODO: function to return word from selected word category
+    word_index = random.randint(0, len(word_list) - 1)
+    return word_list[word_index]
+
+def game_status(missedLetters, correctLetters, secretWord):
+    # TODO: function to return current game status or main UI
+    print(hangman_pic)                                  ######***********
