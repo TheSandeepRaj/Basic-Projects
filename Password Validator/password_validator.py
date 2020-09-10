9666b4f2eff1a78001c0477d357cbba6847581fc
TheSandeepRaj <sandeeprajghimire@gmail.com>
2020-09-10 12:36:56 +0545

    Updated these files

--- Password Validator/password_validator.py
+++ Password Validator/password_validator.py
@@ -3,40 +3,42 @@ Must contain at least one uppercase letter;
 Must contain at least one lowercase letter;
 Must contain at least one number;
 Must contain at least one special character any of ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+']
-'''
+Should not contain spaces'''
 
 def password_checker(password):
 
     special_char = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+']
     
     if len(password) < 8:
-        print("Password must contain at least 8 characters.\n")
+        print("Password must contain at least 8 characters.")
         return False
 
     if not any(char.isupper() for char in password):
-        print('Password must contain at least one uppercase letter.\n')
+        print('Password must contain at least one uppercase letter.')
         return False
 
     if not any(char.islower() for char in password):
-        print('Password must contain at least one lowercase letter.\n')
+        print('Password must contain at least one lowercase letter.')
         return False
 
     if not any(char.isdigit() for char in password):
-        print('Password must contain at least one number.\n')
+        print('Password must contain at least one number.')
         return False
     
     if not any(char in special_char for char in password):
         print("Password must contain at least one special characters.")
-        print("Special Characters :  " + "  ".join(special_char)+ "\n" )
+        print("Special Characters :  " + "  ".join(special_char) )
         return False
     
-    print("Your Password is valid")
+    if any(char.isspace() for char in password):
+        print('Password should not contain spaces.')
+        return False
+
+    print("Your Password is valid.")
     return True
 
 
 
 
-
-
 pwd = input('Enter the password: ')
-password_checker(pwd)
+print(password_checker(pwd))
