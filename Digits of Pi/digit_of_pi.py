4284251f4a4b87e202e3cc999fc880ead960bd10
TheSandeepRaj <sandeeprajghimire@gmail.com>
2020-09-17 22:05:09 +0545

    Updated

---
+++ Digits of Pi/digit_of_pi.py
@@ -0,0 +1,8 @@
+#https://www.craig-wood.com/nick/articles/pi-chudnovsky/
+from sympy.mpmath import mp
+from math import acos 
+#The math.acos() method returns the arc cosine value of a number.\
+
+def printValueOfPi(n):
+    pi = round(2 * acos(0.0), N);
+       
